08845b0434cd2d81674426789243ec4b5965f289
Jean-Aymeric Diet <jeanaymeric@gmail.com>
2022-11-03 11:55:44 +0100

    add AVLTree.java

---
+++ src/com/jad/nfstree/AVLTree.java
@@ -0,0 +1,11 @@
+package com.jad.nfstree;
+
+public class AVLTree<E extends Comparable> extends BinaryTree<E>{
+    public boolean isEquilibrate() {
+        return true;
+    }
+
+    public String toStringWithBalanceFactor() {
+        return "";
+    }
+}
